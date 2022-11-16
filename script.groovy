ee80405acc1a176e3228d7e34ecf71b8de0a41f0
aagatsharma <65018740+aagatsharma@users.noreply.github.com>
2022-11-16 16:40:01 +0545

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar(){
 
 def buildImage(){
     echo "Building docker image..."
-    withCredentials([usernamePassword(credentialsId:'docker-creds',usernameVariable:'USER',passwordVariable:'PWD')]){
+    withCredentials([usernamePassword(credentialsId:'docker-hub-repo',usernameVariable:'USER',passwordVariable:'PWD')]){
         sh 'docker build -t azelmazel/java-maven:1.0 .'
         sh "echo $PWD | docker login -u $USER --password-stdin"
         sh 'docker push azelmazel/java-maven:1.0'
