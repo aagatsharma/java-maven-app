474dbdc90597aa5a6add9e79ed530d03140cf9c9
Aagat <aagat@localhost.localdomain>
2023-01-12 13:18:57 +0545

    Done

--- script.groovy
+++ script.groovy
@@ -31,7 +31,7 @@ def gitbump(){
     withCredentials([usernamePassword(credentialsId: 'github-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'git config --global user.email "jenkins@jenkins.com"'
         sh 'git config --global user.name "jenkins"'
-        sh "git remote set-url origin https://${USER}:${PASS}@github.com/aagatsharma/java-maven-app"
+        sh "git remote set-url origin https://${USER}:${PASS}@github.com/aagatsharma/java-maven-app.git"
         sh 'git add .'
         sh 'git commit -m "CI:Version Bump"'
         sh 'git push origin HEAD:jenkins-shared-library'
